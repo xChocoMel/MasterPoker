74d7d3331f9586a0d44190d5270d6381f3876f80
Jos Raedts <josraedts_109@hotmail.com>
2015-03-23 12:11:15 +0100

    functies loginscherm gedefinieerd
    verder nog niet uitgewerkt!

--- MasterPoker/src/masterpoker/LoginController.java
+++ MasterPoker/src/masterpoker/LoginController.java
@@ -6,18 +6,39 @@
 
 package masterpoker;
 
+import java.awt.Button;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
  *
  * @author Melanie
  */
+
 public class LoginController implements Initializable
 {
+    @FXML Button inlogButton;
+    @FXML Button registerButton;
+    @FXML TextField tfPassword;
+    @FXML TextField tfUsername;
 
+    public void register(){
+        //open register screen
+    }
+    
+    public void login(){
+        if(tfUsername.equals("Melanie") && tfPassword.equals("Brian")){
+            //open Homescreen
+        }
+        else{
+            tfUsername.setText("");
+            tfPassword.setText("");
+        }
+    }
     /**
      * Initializes the controller class.
      */
